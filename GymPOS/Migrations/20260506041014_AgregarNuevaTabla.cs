6642f943279ea5169c93346f85195c6ded8da6e8
yayom <yayomauricio@gmail.com>
2026-05-06 20:53:20 -0600

    s

---
+++ GymPOS/Migrations/20260506041014_AgregarNuevaTabla.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymPOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarNuevaTabla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
